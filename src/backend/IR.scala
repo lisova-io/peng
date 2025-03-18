f979a062b76ebe9c8bbe51b175977f819c3a4b14
rendemare <rendemare@gmail.com>
2025-03-18 23:29:17 +0700

    backending all over compiler

--- src/backend/IR.scala
+++ src/backend/IR.scala
@@ -2,6 +2,7 @@ package backend.ir
 
 import backend.irvalue._
 
+<<<<<<< HEAD
 abstract class Instr()
 
 case class Add(dest: Value, lhs: Value, rhs: Value) extends Instr:
@@ -48,3 +49,31 @@ enum Predicate:
 case class Cmp(dest: Value, pred: Predicate, lhs: Value, rhs: Value) extends Instr:
   override def toString(): String =
     s"$dest = cmp $pred $lhs $rhs"
+=======
+sealed abstract class Instr()
+
+sealed case class Add(lhs: Value, rhs: Value) extends Instr
+
+sealed case class Sub(lhs: Value, rhs: Value) extends Instr
+
+sealed case class Mul(lhs: Value, rhs: Value) extends Instr
+
+sealed case class Div(lhs: Value, rhs: Value) extends Instr
+
+sealed case class Mov(lhs: Value, rhs: Value) extends Instr
+
+sealed case class Call(fn: Value, args: List[Value]) extends Instr
+
+sealed case class Jmp(label: Value) extends Instr
+
+sealed case class Br(cond: Value, tbranch: Value, fbranch: Value) extends Instr
+
+sealed case class Ret(ret: Value) extends Instr
+
+enum Predicate:
+  case Equal
+  case Lt
+  case Gt
+
+sealed case class Cmp(pred: Predicate, lhs: Value, rhs: Value) extends Instr
+>>>>>>> faf60da (backending all over compiler)
