016cdd8b2b9a7542faf8637ae3f9f8c1e8611b90
rendemare <rendemare@gmail.com>
2025-03-19 11:36:08 +0700

    logging and sheise

--- src/backend/IRValue.scala
+++ src/backend/IRValue.scala
@@ -1,24 +1,25 @@
 package backend.irvalue
 
 enum VType:
-  case IInt
-  case IBool
-  case Var
-  case Fn
-  case BBlock
+  case i32
+  case bool
+  case unit
 
 abstract class Value:
   def vtype: VType
   def isConst: Boolean
 
 case class ImmInt(input: Int) extends Value:
-  override def vtype: VType     = VType.IInt
-  override def isConst: Boolean = true
+  override def vtype: VType       = VType.i32
+  override def isConst: Boolean   = true
+  override def toString(): String = s"i32 $input"
 
 case class ImmBool(input: Boolean) extends Value:
-  override def vtype: VType     = VType.IBool
-  override def isConst: Boolean = true
+  override def vtype: VType       = VType.bool
+  override def isConst: Boolean   = true
+  override def toString(): String = s"bool $input"
 
-case class Var(input: String) extends Value:
-  override def vtype: VType     = VType.Var
-  override def isConst: Boolean = false
+case class Var(input: String, vartype: VType) extends Value:
+  override def vtype: VType       = vartype
+  override def isConst: Boolean   = false
+  override def toString(): String = s" $input"
