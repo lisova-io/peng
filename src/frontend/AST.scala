a6fc1fe7237c15a65167ec9b9d11494e4a3a654c
Ilya Sovtsov <ilyasovty@gmail.com>
2025-03-19 23:30:08 +0700

    piece of parser

--- src/frontend/AST.scala
+++ src/frontend/AST.scala
@@ -1,35 +1,41 @@
 package frontend.ast
 
-import scala.collection.immutable.HashMap
+import scala.collection.mutable.HashMap
 
-sealed trait TypeInferrable:
-  def inferType: Type
+type TypeError = String
 
-sealed trait TypeSettable[T]:
-  def setType(t: Type): Either[String, T]
+// sealed trait TypeInferrable:
+//   def inferType: Type
+
+// sealed trait TypeSettable[T]:
+//   def setType(t: Type): Either[TypeError, T]
 
 sealed trait AstNode
-sealed trait Expr extends AstNode with TypeInferrable with TypeSettable[Expr]
-sealed trait Stmt extends AstNode
 sealed trait Decl extends AstNode:
-  def name: Name
+  def getName: Name
+sealed trait Stmt extends AstNode
+sealed trait Expr extends AstNode
 
-type AST  = HashMap[Name, Decl]
-type Name = String
-type Type = String
-class Block(repr: List[Stmt]) extends TypeInferrable with TypeSettable[Block]:
-  override def inferType: Type                         = ???
-  override def setType(t: Type): Either[String, Block] = ???
+type AST   = HashMap[Name, Decl]
+type Name  = String
+type Type  = String
+type Block = List[Stmt]
 
-class FnDecl(val name: Name, val args: List[(Name, Type)], val tp: Type, val body: Block)
-    extends Decl
-class VarDecl(val name: Name, val tp: Option[Type], val value: Expr) extends Decl
+sealed class FnDecl(val name: Name, val args: List[(Name, Type)], val tp: Type, val body: Block)
+    extends Decl {
+  override def getName: Name = name
+}
 
-class ExprStmt(val expr: Expr)        extends Stmt
-class BlockStmt(val block: Block)     extends Stmt
-class DeclStmt(val decl: Decl)        extends Stmt
-class RetStmt(val expr: Option[Expr]) extends Stmt
-class VoidRetStmt                     extends Stmt
+sealed class VarDecl(val const: Boolean, val name: Name, val tp: Option[Type], val value: Expr)
+    extends Decl {
+  override def getName: Name = name
+}
+
+sealed class ExprStmt(val expr: Expr)        extends Stmt
+sealed class BlockStmt(val block: Block)     extends Stmt
+sealed class DeclStmt(val decl: Decl)        extends Stmt
+sealed class RetStmt(val expr: Option[Expr]) extends Stmt
+sealed class VoidRetStmt                     extends Stmt
 
 enum UnaryOp:
   case Minus
@@ -38,18 +44,8 @@ enum BinOp:
   case Plus
   case Minus
 
-class VarRefExpr(val name: Name) extends Expr:
-  override def inferType: Type                              = ???
-  override def setType(t: Type): Either[String, VarRefExpr] = ???
-class CallExpr(val name: Name, val args: List[Expr]) extends Expr:
-  override def inferType: Type                            = ???
-  override def setType(t: Type): Either[String, CallExpr] = ???
-class BinExpr(val op: BinOp, val lhs: Expr, val rhs: Expr) extends Expr:
-  override def inferType: Type                           = ???
-  override def setType(t: Type): Either[String, BinExpr] = ???
-class UnaryExpr(val op: UnaryOp, val expr: Expr) extends Expr:
-  override def inferType: Type                             = ???
-  override def setType(t: Type): Either[String, UnaryExpr] = ???
-class NumLitExpr(val tp: Type, val name: Name) extends Expr:
-  override def inferType: Type                              = ???
-  override def setType(t: Type): Either[String, NumLitExpr] = ???
+sealed class VarRefExpr(val name: Name)                           extends Expr
+sealed class CallExpr(val name: Name, val args: List[Expr])       extends Expr
+sealed class BinExpr(val op: BinOp, val lhs: Expr, val rhs: Expr) extends Expr
+sealed class UnaryExpr(val op: UnaryOp, val expr: Expr)           extends Expr
+sealed class NumLitExpr(val tp: Type, val name: Name)             extends Expr
--- src/frontend/Parse.scala
+++ src/frontend/Parse.scala
@@ -5,6 +5,9 @@ import cats.parse.Rfc5234._
 import cats.data.NonEmptyList
 import cats.parse.Parser.Expectation
 import cats.parse.Parser.Expectation._
+import cats.parse.Parser.Error
+
+import scala.collection.mutable.HashMap
 
 import frontend.ast._
 
@@ -24,17 +27,38 @@ def identifier: Parser[String] =
 def number: Parser[Int] = symbol(digit.rep).map(x => intFromDigits(x.map(_.toInt - '0').toList))
 def token(ch: Char)     = symbol(Parser.char(ch))
 def token(s: String)    = symbol(Parser.string(s))
-def tp: Parser[Type]    = identifier
+def parens[T](p: Parser[T]): Parser[T]   = p.between(token('('), token(')'))
+def parens[T](p: Parser0[T]): Parser0[T] = p.between(token('('), token(')'))
+def braces[T](p: Parser[T]): Parser[T]   = p.between(token('{'), token('}'))
+def braces[T](p: Parser0[T]): Parser0[T] = p.between(token('{'), token('}'))
+def tp: Parser[Type]                     = identifier
 
-def expr: Parser[Expr] = ???
+def expr: Parser[Expr]    = ???
+def block: Parser0[Block] = braces(stmt.repSep0(token(';')))
 
-def varDecl: Parser[VarDecl] =
-  def mkVarDecl(args: ((String, Option[Type]), Expr)): VarDecl = {
-    val ((name, t), value) = args
-    VarDecl(name, t, value)
-  }
-  (identifier ~ (token(':') *> tp).? ~ (token('=') *> expr)).map(mkVarDecl)
+def varDecl: Parser[VarDecl] = {
+  val isConst: Parser[Boolean] = token("val").as(true).backtrack | token("var").as(false)
+
+  (isConst ~ identifier ~ (token(':') *> tp).backtrack.? ~ (token('=') *> expr))
+    .map(in => {
+      val (((const, name), t), value) = in
+      VarDecl(const, name, t, value)
+    })
+}
+
+def fnDecl: Parser[FnDecl] =
+  def fnArg: Parser[(Name, Type)] = identifier ~ (token(':') *> tp)
+
+  (token("fn") *> identifier ~ parens(fnArg.repSep0(token(','))) ~ (token(
+    ':'
+  ) *> tp) ~ block)
+    .map(in => {
+      val (((name, args), rettype), body) = in
+      FnDecl(name, args, rettype, body)
+    })
 
 def stmt: Parser[Stmt] = varDecl.map(DeclStmt(_)).backtrack | ???
+def decl: Parser[Decl] = varDecl.backtrack | fnDecl
 
-// def parse(): Parser[AST] =
+def parse(input: String): Either[Error, AST] =
+  decl.rep0.parse(input).map(r => HashMap().addAll(r._2.map(d => d.getName -> d)))
