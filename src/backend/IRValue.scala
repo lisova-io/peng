b33c00e1009b2ea9d580e39694b4df1e9d91175d
rendemare <rendemare@gmail.com>
2025-03-19 11:36:08 +0700

    logging and sheise

--- src/backend/IRValue.scala
+++
@@ -1,9 +0,0 @@
-package backend.irvalue
-
-abstract class Value
-
-sealed case class ImmInt(input: Int) extends Value
-
-sealed case class ImmBool(input: Boolean) extends Value
-
-sealed case class Var(input: String) extends Value
