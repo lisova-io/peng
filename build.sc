6f7caec0281c84114c1a77d8b931f616698216ce
Ilya Sovtsov <ilyasovty@gmail.com>
2025-03-22 23:29:08 +0700

    parser?

--- build.sc
+++ build.sc
@@ -4,7 +4,7 @@ import mill._, scalalib._
 object `package` extends RootModule with ScalaModule {
   def scalaVersion = "3.6.4"
 
-  def ivyDeps = Agg(
-    ivy"org.typelevel::cats-parse::1.0.0"
-  )
+  // def ivyDeps = Agg(
+  // ivy"org.typelevel::cats-parse::1.0.0"
+  // )
 }
--- src/frontend/Parse.scala
+++ src/frontend/Parse.scala
@@ -1,64 +1,86 @@
 package frontend.parse
 
-import cats.parse.{Parser, Parser0}
-import cats.parse.Rfc5234._
-import cats.data.NonEmptyList
-import cats.parse.Parser.Expectation
-import cats.parse.Parser.Expectation._
-import cats.parse.Parser.Error
-
+import frontend.ast._
+import frontend.lex.{Lexer, Offset, Token, Span, WithSpan}
 import scala.collection.mutable.HashMap
 
-import frontend.ast._
+class ParseError(val span: Span, val msg: String):
+  def unapply: (Span, String) = (span, msg)
 
-private def intFromDigits(digits: Seq[Int]): Int = {
-  var res = 0
-  for (digit <- digits) {
-    res *= 10
-    res += digit
+type ParseResult[T] = Either[List[ParseError], T]
+
+private def unexpected[A, E](span: Span, actual: A, expected: E*): ParseError = {
+  def formatExpected(l: Seq[E]): String =
+    l match
+      case Nil           => ""
+      case x :: Nil      => x.toString
+      case x :: y :: Nil => s"$x or $y"
+      case x :: tail     => s"$x, " + formatExpected(tail)
+
+  ParseError(span, s"Unexpected $actual. Expected ${formatExpected(expected)}")
+}
+
+class Parser(lexer: Lexer):
+  def token[Tok](tok: Tok): ParseResult[Unit] =
+    lexer.next
+      .map(_ match
+        case ws @ WithSpan(tok, s) => Right(()))
+      .getOrElse(Left(List(unexpected(lexer.endSpan, "eof", "variable declaration"))))
+
+  def token[Tok, T](matcher: Token => Boolean, fn: Tok => T): ParseResult[WithSpan[T]] = ???
+
+  def parseFnDecl: ParseResult[FnDecl] = ???
+
+  def parseType: ParseResult[WithSpan[Type]] = ???
+
+  def parseExpr: ParseResult[Expr] = ???
+
+  def parseVarDecl(const: Boolean): ParseResult[FnDecl] = {
+    val const = token(_ == Token.Val, _ => true).orElse(token(_ == Token.Var, _ => false))
+    val name = token[Token.Identifier, Name](
+      (_ == Token.Identifier),
+      (_.value)
+    )
+
+    val typeResult: ParseResult[Option[WithSpan[Type]]] =
+      lexer.peek
+        .map(ws =>
+          ws match
+            case WithSpan(Token.Colon, _) => token(Token.Colon).flatMap(_ => parseType).map(Some(_))
+            case WithSpan(Token.Assign, _) => Right(None)
+            case WithSpan(tok, span) => Left(List(unexpected(span, tok, "variable declaration")))
+        )
+        .getOrElse(Left(List(unexpected(lexer.endSpan, "eof", "variable declaration"))))
+
+    const.flatMap(const =>
+      name.flatMap(name =>
+        typeResult.flatMap(t =>
+          // todo: value
+          token(Token.Assign)
+            .flatMap(_ => parseExpr)
+            .map(VarDecl(const.value, name, t)) // got concrete type
+        )
+      )
+    )
   }
-  return res
-}
 
-def whitespace0: Parser0[Unit]         = Parser.charIn(" \t\r\n").rep0.void
-def symbol[T](p: Parser[T]): Parser[T] = p.surroundedBy(whitespace0)
-def identifier: Parser[String] =
-  symbol(alpha ~ (alpha.backtrack | digit).rep.string).map((c, s) => c + s)
-def number: Parser[Int] = symbol(digit.rep).map(x => intFromDigits(x.map(_.toInt - '0').toList))
-def token(ch: Char)     = symbol(Parser.char(ch))
-def token(s: String)    = symbol(Parser.string(s))
-def parens[T](p: Parser[T]): Parser[T]   = p.between(token('('), token(')'))
-def parens[T](p: Parser0[T]): Parser0[T] = p.between(token('('), token(')'))
-def braces[T](p: Parser[T]): Parser[T]   = p.between(token('{'), token('}'))
-def braces[T](p: Parser0[T]): Parser0[T] = p.between(token('{'), token('}'))
-def tp: Parser[Type]                     = identifier
+  def parse: ParseResult[AST] = {
+    var res: AST                 = HashMap[Name, Decl]()
+    var errors: List[ParseError] = List()
 
-def expr: Parser[Expr]    = ???
-def block: Parser0[Block] = braces(stmt.repSep0(token(';')))
+    while lexer.peek.isDefined do {
+      val declRes = lexer.next.get match
+        case WithSpan(Token.Fn, _)  => parseFnDecl
+        case WithSpan(Token.Val, _) => parseVarDecl(const = true)
+        case WithSpan(Token.Var, _) => parseVarDecl(const = false)
+        case WithSpan(t, s)         => Left(List(unexpected(s, t, "declaration")))
 
-def varDecl: Parser[VarDecl] = {
-  val isConst: Parser[Boolean] = token("val").as(true).backtrack | token("var").as(false)
+      declRes match
+        case Left(errs) => errors ++= errs
+        // TODO: handle name duplication
+        // case Right(decl) if res.contains(decl.getName) => errors :+= ParseError(i)
+        case Right(decl) => res += (decl.name.value, decl)
+    }
 
-  (isConst ~ identifier ~ (token(':') *> tp).backtrack.? ~ (token('=') *> expr))
-    .map(in => {
-      val (((const, name), t), value) = in
-      VarDecl(const, name, t, value)
-    })
-}
-
-def fnDecl: Parser[FnDecl] =
-  def fnArg: Parser[(Name, Type)] = identifier ~ (token(':') *> tp)
-
-  (token("fn") *> identifier ~ parens(fnArg.repSep0(token(','))) ~ (token(
-    ':'
-  ) *> tp) ~ block)
-    .map(in => {
-      val (((name, args), rettype), body) = in
-      FnDecl(name, args, rettype, body)
-    })
-
-def stmt: Parser[Stmt] = varDecl.map(DeclStmt(_)).backtrack | ???
-def decl: Parser[Decl] = varDecl.backtrack | fnDecl
-
-def parse(input: String): Either[Error, AST] =
-  decl.rep0.parse(input).map(r => HashMap().addAll(r._2.map(d => d.getName -> d)))
+    if errors.isEmpty then Right(res) else Left(errors)
+  }
