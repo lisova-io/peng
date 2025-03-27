69bb53f66bf035e841eddb6ccf2601d91c299ac5
rendemare <rendemare@gmail.com>
2025-03-27 17:27:20 +0700

    intermediate stuff

--- src/backend/Control.scala
+++ src/backend/Control.scala
@@ -2,17 +2,20 @@ package backend.control
 
 import backend.ir._
 import backend.irvalue._
+import backend.irgen.irbuilder._
 
 trait ControlFlow
 
-class Program extends ControlFlow
+class Program(fns: Vector[Function]) extends ControlFlow
 
 case class BasicBlock(name: String, instrs: Vector[Instr]) extends Value with ControlFlow:
   override def vtype: VType     = VType.unit // idk
   override def isConst: Boolean = false
-  override def toString(): String =
+  override def toString: String =
     s"$name:" + instrs.foldLeft((acc: String, instr: Instr) => acc + instr + "\n")
 
-case class Function(rtype: VType, name: String, args: List[Value]) extends Value with ControlFlow:
+case class Function(blocks: Vector[BasicBlock], rtype: VType, name: String, args: List[Value])
+    extends Value
+    with ControlFlow:
   override def vtype: VType     = rtype
   override def isConst: Boolean = false
