7db850d4963e56f6489d025fcdf2dee9ac6a5d60
rendemare <rendemare@gmail.com>
2025-03-29 21:35:54 +0700

    Logging PassManager

--- src/backend/opt/PassManager.scala
+++ src/backend/opt/PassManager.scala
@@ -2,10 +2,11 @@ package backend.opt.passmanager
 
 import scala.collection.mutable.HashMap
 import backend.ir.control._
+import com.typesafe.scalalogging.StrictLogging
 
 type Program = HashMap[String, Function]
 
-trait PassManager:
+sealed trait PassManager:
   def addPass(pass: Pass): PassManager
   def perform: Program
 
@@ -16,26 +17,26 @@ trait LocalPass extends Pass:
 trait GlobalPass extends Pass:
   def pass(fn: Function): Function
 
-class DefaultManager(program: Program) extends PassManager:
+sealed class DefaultManager(program: Program) extends PassManager:
   var current_program    = program
   var passes: List[Pass] = List()
   def addPass(pass: Pass): DefaultManager =
     passes :+= pass
     this
-  private def localPass(pass: LocalPass): Program =
+  protected def localPass(pass: LocalPass): Program =
     program
       .map((name, fn) => {
         val new_blocks = fn.blocks.map(pass.pass(_))
         (name, fn.copy(blocks = new_blocks))
       })
 
-  private def globalPass(pass: GlobalPass): Program =
+  protected def globalPass(pass: GlobalPass): Program =
     program
       .map((name, fn) => {
         (name, pass.pass(fn))
       })
 
-  private def performPass(pass: Pass): Program =
+  protected def performPass(pass: Pass): Program =
     pass match
       case lp: LocalPass  => localPass(lp)
       case gp: GlobalPass => globalPass(gp)
@@ -43,3 +44,14 @@ class DefaultManager(program: Program) extends PassManager:
   def perform: Program =
     for (pass <- passes) do current_program = performPass(pass)
     current_program
+
+final class LoggingManager(program: Program) extends DefaultManager(program) with StrictLogging:
+  private def logPass[T](pass: Pass, res: => T): T =
+    logger.debug(s"BEFORE ${pass}")
+    logger.debug(s"$program")
+    val result = res
+    logger.debug(s"AFTER ${pass}")
+    logger.debug(s"$result")
+    result
+  override protected def performPass(pass: Pass): Program =
+    logPass(pass, super.performPass(pass))
